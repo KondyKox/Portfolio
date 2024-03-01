a94e5ca2fbd53e2d9effe0acbb93f6e9a6a22cdb
Konrad <kondykox@gmail.com>
2024-03-02 00:15:54 +0100

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { I18nextProvider, useTranslation } from "react-i18next";
 import i18n from "./i18n";
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+
+// Get language from local storage
+const getLanguage = () => {
+  const storedLanguage = localStorage.getItem("language") || "en";
+  return storedLanguage;
+};
 
 const App = () => {
   const { t } = useTranslation();
+  const [language, setLanguage] = useState(getLanguage());
+
+  useEffect(() => {
+    const language = getLanguage();
+    i18n.changeLanguage(language);
+    setLanguage(language);
+  }, []);
 
   return (
-    <I18nextProvider>
-      <div></div>
+    <I18nextProvider i18n={i18n}>
+      <Navbar t={t} language={language} setLanguage={setLanguage} />
     </I18nextProvider>
   );
 };
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,105 @@
+import { useEffect, useState } from "react";
+import "./Navbar.css";
+import i18n from "../../i18n";
+
+const Navbar = ({ t, language, setLanguage }) => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+  }, []);
+
+  // Choose your language
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+    localStorage.setItem("language", lng);
+    setLanguage(lng);
+  };
+
+  // Toggle menu
+  const handleToggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <header className="container">
+      {/* Mobile navbar */}
+      {windowWidth <= 768 ? (
+        <>
+          <button className="nav__btn" onClick={handleToggleMenu}>
+            ☰
+          </button>
+          {isMenuOpen && windowWidth < 768 ? (
+            <nav className="mobile__nav">
+              <div className="nav__items">
+                <ul className="nav__items-list">
+                  <li className="nav__item">
+                    <a href="#">{t("navbar.about")}</a>
+                  </li>
+                  <li className="nav__item">
+                    <a href="#">{t("navbar.projects")}</a>
+                  </li>
+                  <li className="nav__item">
+                    <a href="#">{t("navbar.contact")}</a>
+                  </li>
+                </ul>
+              </div>
+              <div className="nav__languages">
+                <img
+                  src="./flags/en.png"
+                  alt="English"
+                  onClick={() => changeLanguage("en")}
+                  className={language === "en" ? "active" : null}
+                />
+                <img
+                  src="./flags/pl.png"
+                  alt="Polski"
+                  onClick={() => changeLanguage("pl")}
+                  className={language === "pl" ? "active" : null}
+                />
+              </div>
+            </nav>
+          ) : null}
+        </>
+      ) : (
+        // Normal navbar
+        <nav>
+          <div className="nav__items">
+            <ul className="nav__items-list">
+              <li className="nav__item">
+                <a href="#">{t("navbar.about")}</a>
+              </li>
+              <li className="nav__item">
+                <a href="#">{t("navbar.projects")}</a>
+              </li>
+              <li className="nav__item">
+                <a href="#">{t("navbar.contact")}</a>
+              </li>
+            </ul>
+          </div>
+          <div className="nav__languages">
+            <img
+              src="./flags/en.png"
+              alt="English"
+              onClick={() => changeLanguage("en")}
+              className={language === "en" ? "active" : null}
+            />
+            <img
+              src="./flags/pl.png"
+              alt="Polski"
+              onClick={() => changeLanguage("pl")}
+              className={language === "pl" ? "active" : null}
+            />
+          </div>
+        </nav>
+      )}
+    </header>
+  );
+};
+
+export default Navbar;
